ae37c895a8eb916d5c8d03aacdeba55f4e762a31
Greg Bowler <greg.bowler@g105b.com>
2021-03-23 15:01:11 +0000

    build: static analysis fixes
    * refactor: logic order for test coverage
    
    * chore: upgrade phpunit
    
    * chore: fix static analysis errors
    
    * chore: fix static analysis errors
    
    * build: upgrade deps

--- src/Rule/TypeNumber.php
+++ src/Rule/TypeNumber.php
@@ -4,79 +4,82 @@ namespace Gt\DomValidation\Rule;
 use DOMElement;
 
 class TypeNumber extends Rule {
+	/** @var string[] */
 	protected array $attributes = [
 		"type=number",
 		"type=range",
 	];
 
 	public function isValid(DOMElement $element, string $value):bool {
-		$min = $element->getAttribute("min") ?? null;
-		$max = $element->getAttribute("max") ?? null;
-		$step = $element->getAttribute("step") ?? null;
+		$min = $element->getAttribute("min") ?: null;
+		$max = $element->getAttribute("max") ?: null;
+		$step = $element->getAttribute("step") ?: null;
 
-		if(empty($value)) {
-			return true;
+		if($value === "") {
+			$validity = true;
 		}
+		elseif(is_numeric($value)) {
+			$value = (float)$value;
 
-		if(!is_numeric($value)) {
-			return false;
-		}
-
-		$value = (float)$value;
-
-		if($min !== ""
-		&& $value < $min) {
-			return false;
-		}
-
-		if($max !== ""
-		&& $value > $max) {
-			return false;
-		}
-
-		if($step !== "") {
-			if($min) {
-				return ($value - $min) % $step === 0;
+			if(!is_null($min)
+			&& $value < $min) {
+				$validity = false;
 			}
+			elseif(!is_null($max)
+			&& $value > $max) {
+				$validity = false;
+			}
+			elseif(!is_null($step)) {
+				if($min) {
+					$validity = ($value - $min) % $step === 0;
+				}
+				else {
+					$validity = $value % $step === 0;
+				}
 
-			return $value % $step === 0;
+			}
+			else {
+				$validity = true;
+			}
+		}
+		else {
+			$validity = false;
 		}
 
-		return true;
+		return $validity;
 	}
 
 	public function getHint(DOMElement $element, string $value):string {
-		$min = $element->getAttribute("min") ?? null;
-		$max = $element->getAttribute("max") ?? null;
-		$step = $element->getAttribute("step") ?? null;
+		$min = $element->getAttribute("min") ?: null;
+		$max = $element->getAttribute("max") ?: null;
+		$step = $element->getAttribute("step") ?: null;
+		$hint = "";
 
-		if(!is_numeric($value)) {
-			return "Field must be a number";
-		}
+		if(is_numeric($value)) {
+			$value = (float)$value;
 
-		$value = (float)$value;
-
-		if($min !== ""
-		&& $value < $min) {
-			return "Field value must not be less than $min";
-		}
-
-		if($max !== ""
-		&& $value > $max) {
-			return "Field value must not be greater than $max";
-		}
-
-		if(!empty($step)) {
-			if($min
-			&& ($value - $min) % $step !== 0) {
-				return "Field value must be $min plus a multiple of $step";
+			if(!is_null($min)
+			&& $value < $min) {
+				$hint = "Field value must not be less than $min";
 			}
-
-			if($value % $step !== 0) {
-				return "Field value must be a multiple of $step";
+			elseif(!is_null($max)
+			&& $value > $max) {
+				$hint = "Field value must not be greater than $max";
+			}
+			elseif(!is_null($step)) {
+				if(!is_null($min)
+				&& ($value - $min) % $step !== 0) {
+					$hint = "Field value must be $min plus a multiple of $step";
+				}
+				elseif($value % $step !== 0) {
+					$hint = "Field value must be a multiple of $step";
+				}
 			}
 		}
+		else {
+			$hint = "Field must be a number";
+		}
 
-		return "Field must be a number";
+		return $hint;
 	}
 }
